9f9bb5e599098eedeae2b81c8809b86e643dc12d
Gaston Gutierrez Llomplat <gastong7337@gmail.com>
2025-03-25 16:14:03 -0900

    Punto 3e

--- tp1_1.c
+++ tp1_1.c
@@ -3,7 +3,25 @@
 int main() {
 
     printf("Hola Mundo\n");
-    printf("Clase de Taller 1");
+    printf("Clase de Taller 1\n\n");
+
+    int numero = 25;
+    int *p_numero = &numero;
+
+    printf("Contenido del puntero: ");
+    printf("%d\n", *p_numero);
+
+    printf("Direccion de memoria almacenada por el puntero: ");
+    printf("%d\n", p_numero);
+
+    printf("Direccion de memoria de numero: ");
+    printf("%d\n", &numero);
+
+    printf("Direccion de memoria del puntero: ");
+    printf("%d\n", &p_numero);
+
+    printf("Cantidad de memoria utilizada por la variable: ");
+    printf("%d", sizeof(numero));
 
     return 0;
 } 
